f21eee8311b41d0d26dcb2a257ffd3aaec399ecd
Kyle DeGuzman <kyle.deguzman.579@my.csun.edu>
2022-01-14 16:39:19 -0800

    new sidebar

---
+++ index.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("sideMenu").style.width = "100%";
+    document.getElementById("main").style.display = "none";
+  }
+  
+  function closeNav() {
+    document.getElementById("sideMenu").style.width = "0";
+    document.getElementById("main").style.display= "block";
+  }
